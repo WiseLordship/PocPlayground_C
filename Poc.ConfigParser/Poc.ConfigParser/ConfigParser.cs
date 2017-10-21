75c3c169d096177e06f932e942c38d8ea7b64cc1
Wiselordship <Wiselordship@gmail.com>
2017-10-20 21:29:55 -0500

    Initial load

---
+++ Poc.ConfigParser/Poc.ConfigParser/ConfigParser.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Poc.ConfigParser
+{
+    public class ConfigParser
+    {
+        public XmlDocument GetConfigDocument(string fileLocation)
+        {
+            if (!File.Exists(fileLocation)) throw new FileNotFoundException();
+
+            using (var reader = new StreamReader(
+                new FileStream(
+                    fileLocation,
+                    FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.Read)
+            ))
+            {
+                var doc = new XmlDocument();
+                var xmlIn = reader.ReadToEnd();
+                reader.Close();
+                doc.LoadXml(xmlIn);
+                return doc;
+            }
+        }
+
+        public Dictionary<string, string> GetAppSettings(XmlDocument file)
+        {
+            var dictionary = new Dictionary<string, string>();
+            var configurationNode = file.SelectSingleNode("/configuration");
+            foreach (XmlNode child in configurationNode.ChildNodes)
+                if (child.Name.Equals("appSettings"))
+                    foreach (XmlNode node in child.ChildNodes)
+                        if (node.Name.Equals("add"))
+                            dictionary.Add
+                            (
+                                node.Attributes["key"].Value,
+                                node.Attributes["value"].Value
+                            );
+            return dictionary;
+        }
+
+
+        public Dictionary<string, string> GetConnectionSettings(XmlDocument file)
+        {
+            var dictionary = new Dictionary<string, string>();
+            var configurationNode = file.SelectSingleNode("/configuration");
+            foreach (XmlNode child in configurationNode.ChildNodes)
+                if (child.Name.Equals("connectionStrings"))
+                    foreach (XmlNode node in child.ChildNodes)
+                        if (node.Name.Equals("add"))
+                            dictionary.Add
+                            (
+                                node.Attributes["name"].Value,
+                                node.Attributes["connectionString"].Value
+                            );
+            return dictionary;
+        }
+
+        public XmlNode GetConfigSecion(XmlDocument file)
+        {
+            var configurationNode = file.SelectSingleNode("/configuration");
+            foreach (XmlNode child in configurationNode.ChildNodes)
+                if (child.Name.Equals("watchGuard.config"))
+                    return child;
+            return null;
+        }
+    }
+}
---
+++ Poc.ConfigParser/Poc.ConfigParser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Poc.ConfigParser
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var parser = new ConfigParser();
+            var testFilePAth = $"{Directory.GetCurrentDirectory()}\\App1.config";
+
+            var configDocument = parser.GetConfigDocument(testFilePAth);
+            var appSettings = parser.GetAppSettings(configDocument);
+            var connectionStrings = parser.GetConnectionSettings(configDocument);
+
+            Console.WriteLine("Appsettings -----------------------------------");
+            foreach (var appSetting in appSettings)
+            {
+                Console.WriteLine($"{appSetting.Key} : {appSetting.Value}");
+            }
+
+            Console.WriteLine("Connectionsettings -----------------------------");
+            foreach (var connecionSetting in connectionStrings)
+            {
+                Console.WriteLine($"{connecionSetting.Key} : {connecionSetting.Value}");
+            }
+
+            var specialNode = parser.GetConfigSecion(configDocument);
+            Console.WriteLine("Custom Section ----------------------------------");
+            Console.WriteLine($"{specialNode.OuterXml}");
+
+            Console.WriteLine("Done");
+            Console.Read();
+        }
+    }
+}
